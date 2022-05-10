d6e80ce9b2c44219d4975cc2736ebf8bc5840c40
Sarah Wilkes <aremnius@gmail.com>
2022-05-10 14:41:12 -0400

    display dummy data on page

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-// some fake data
+const input = document.querySelector('#prompt_input');
+const submitButton = document.querySelector('#prompt_submit');
+const output = document.querySelector('#prompt_output');
+
+let userQuery;
+let aiResponses = [];
 
 
-// sample reply from docs
 
+// sample/working draft request
+const data = {
+  // more detailed instructions to the API would go here
+  "prompt": "Take on me",
+  "max_tokens": 20,
+  "temperature": 0,
+  "n": 1,
+  "echo": true,
+}
+
+
+// sample reply based on docs
 const sampleReply = {
   "id": "cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7",
   "object": "text_completion",
@@ -10,23 +26,51 @@ const sampleReply = {
   "model": "text-curie-001",
   "choices": [
     {
-      "text": "\n\nThis is a test",
+      "prompt": "Take on me",
+      "text": "Take me on",
       "index": 0,
       "logprobs": null,
       "finish_reason": "length"
     }
   ]
 }
-// add echo. Will have to guess at the key/name of it for testing, but it would be in "choices"
+// adding echo, but I don't know the exact format until I test the API
+
+// async function fetchReply(data) {
+//   const reply = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+//   },
+//   body: JSON.stringify(data),
+//   });
+//   const aiResponse = await reply.json(); 
+//   addResponse(aiResponse);
+// }
 
 
-const submitButton = document.querySelector('#prompt_submit');
-const input = document.querySelector('#prompt_input');
+function displayResponses(aiResponse) {
+  aiResponses.unshift(aiResponse);
+  const html = aiResponses.map(
+    response => `<div class="prompt_and_response"><p class="prompt_label">Prompt:</p>
+    <p>${response[0].prompt}</p>
+    <p class="response_label">Response:</p>
+    <p>${response[0].text}</p></div>`
+  )
+  output.innerHTML = html.join('');
+}
 
 function handleSubmit(e) {
   e.preventDefault();
-  const userQuery = e.target.form.prompt_input.value;
+  let userQuery = e.target.form.prompt_input.value;
   console.log(userQuery);
 }
 
 submitButton.addEventListener('click', handleSubmit);
+
+displayResponses(sampleReply.choices);
+displayResponses(sampleReply.choices);
+displayResponses(sampleReply.choices);
+
+console.log(aiResponses);
