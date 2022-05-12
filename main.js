76ed3ae22bbd47e1ae697888f25d991636073473
Sarah Wilkes <aremnius@gmail.com>
2022-05-12 09:53:18 -0400

    JS working

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ const submitButton = document.querySelector('#prompt_submit');
 const output = document.querySelector('#prompt_output');
 
 
-// let userQuery;
-// static for testing
-let userQuery = "I'm a little teapot";
+let userQuery;
 let aiResponses = [];
 
 
-// sample reply based on docs
+// sample reply
 const sampleReply = {
   "id": "cmpl-uqkvlQyYK7bGYrRHQ0eXlWi7",
   "object": "text_completion",
@@ -18,7 +16,7 @@ const sampleReply = {
   "model": "text-curie-001",
   "choices": [
     {
-      "text": "Take me on",
+      "text": "\n\nThat grows in the sun\n\nMy petals are soft\n\nAnd they're proudly",
       "index": 0,
       "logprobs": null,
       "finish_reason": "length"
@@ -26,34 +24,33 @@ const sampleReply = {
   ]
 }
 
-// this part works
-// async function fetchReply(userQuery) {
-//   const data = {
-//     "prompt": `Provide the next line of a song lyric. ${userQuery}`,
-//     "max_tokens": 42,
-//     "temperature": 0.9,
-//   }
-//   const reply = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       // Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
-//       // the above is most common but inside Vite it will probably be:
-//       Authorization: `Bearer ${import.meta.env.VITE_OPENAI_SECRET}`,
-//     },
-//     body: JSON.stringify(data),
-//   });
-//   const aiResponse = await reply.json(); 
-//   console.log(aiResponse);
-//   displayResponses(aiResponse.choices, userQuery);
-// }
+async function fetchReply(userQuery) {
+  const data = {
+    "prompt": `Provide the next line of a song lyric. ${userQuery}`,
+    "max_tokens": 42,
+    "temperature": 0.9,
+  }
+  const reply = await fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+      // the above is most common but inside Vite it will probably be:
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI_SECRET}`,
+    },
+    body: JSON.stringify(data),
+  });
+  const aiResponse = await reply.json(); 
+  aiResponse.choices.userQuery = userQuery;
+  console.log(aiResponse);
+  displayResponses(aiResponse.choices);
+}
 
-// I need to add userQuery to the saved object
-function displayResponses(aiResponse, userQuery) {
+function displayResponses(aiResponse) {
   aiResponses.unshift(aiResponse);
   const html = aiResponses.map(
     response => `<div class="prompt_and_response"><p class="prompt_label">Prompt:</p>
-    <p>${userQuery}</p>
+    <p>${response.userQuery}</p>
     <p class="response_label">Response:</p>
     <p class="newline">${response[0].text}</p></div>`
   )
@@ -68,10 +65,6 @@ function handleSubmit(e) {
 }
 
 submitButton.addEventListener('click', handleSubmit);
-;
 
-// example text reply I'm getting 
-// "\n\nThat grows in the sun\n\nMy petals are soft\n\nAnd they're proudly"
 
-displayResponses(sampleReply.choices, userQuery);
 console.log(aiResponses);
